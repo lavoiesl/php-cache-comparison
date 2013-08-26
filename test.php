7bc48ef53badf7b5abd02e92b16cca0ee240cbf7
Sébastien Lavoie <github@lavoie.sl>
2013-08-26 16:46:43 -0400

    updated with new doctrine-cache-detector

--- test.php
+++ test.php
@@ -4,38 +4,24 @@ require 'vendor/autoload.php';
 
 use Lavoiesl\PhpBenchmark\Benchmark;
 use Lavoiesl\PhpCacheComparison\Test;
+use Lavoiesl\Doctrine\CacheDetector\CacheChooser;
+use Lavoiesl\Doctrine\CacheDetector\Detector\AbstractDetector;
+use Lavoiesl\Doctrine\CacheDetector\Detector\ApcDetector;
 use Doctrine\Common\Cache;
 
-if (!function_exists('apc_fetch') && function_exists('apcu_fetch')) {
-    require __DIR__ . '/fix_apcu.php';
-}
+ApcDetector::createAPCuAliases();
+
+$chooser = new CacheChooser;
+$detectors = $chooser->getSupportedDetectors(AbstractDetector::PERSISTANCE_LOCAL_SERVICE);
 
 $benchmark = new Benchmark;
 
-
-$caches = array(
-    // 'apc' => new Cache\ApcCache(),
-    'file' => new Cache\FilesystemCache('/tmp/filesystemcache'),
-    'phpfile' => new Cache\PhpFileCache('/tmp/phpfilecache'),
-    'memcache' => new Cache\MemcacheCache(),
-    'memcached' => new Cache\MemcachedCache(),
-    // 'redis' => new Cache\RedisCache(),
-    // 'xcache' => new Cache\XcacheCache(),
-);
-
-$memcache = new Memcache;
-$memcache->connect('localhost', 11211);
-$caches['memcache']->setMemcache($memcache);
-
-$memcached = new Memcached;
-$memcached->addServer('localhost', 11211);
-$caches['memcached']->setMemcached($memcached);
-
-foreach ($caches as $cache) {
+foreach ($detectors as $detector) {
+    $cache = $detector->getCache();
     $benchmark->addTest(new Test\ReadTest($cache));
     $benchmark->addTest(new Test\WriteTest($cache));
     $benchmark->addTest(new Test\ReadWriteTest($cache));
 }
 
-$benchmark->guessCount(5);
+@$benchmark->guessCount(1);
 $benchmark->run();
